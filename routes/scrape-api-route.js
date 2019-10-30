bebc355339ff3f980e3f79aada0a6bf9c7c05bb9
Myles Carey <seanmyles@comcast.net>
2019-10-30 14:40:19 -0700

    scrap route working

--- routes/scrape-api-route.js
+++ routes/scrape-api-route.js
@@ -5,7 +5,11 @@
 // Dependencies
 // =============================================================
 var db = require("../models");
-
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
 
 // Routes
 // =============================================================
@@ -13,36 +17,53 @@ var db = require("../models");
 module.exports = function (app) {
 
   // A GET route for scraping the website
-  app.get("/scrape", function(req, res) {
+  app.get("/api/scrape", function(req, res) {
     // First, we grab the body of the html with axios
-    axios.get("http://www.echojs.com/").then(function(response) {
+    axios.get("https://www.latimes.com").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
-
+      var blogCount = 0;
       // Now, we grab every h2 within an article tag, and do the following:
-      $("article h2").each(function(i, element) {
+      $("div.PromoSmall-content").each(function(i,element) {
         // Save an empty result object
+        
         var result = {};
+        blogCount++;
+        if (blogCount > 20) {
+          return false;
+        };
 
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-          .children("a")
-          .text();
-        result.link = $(this)
-          .children("a")
-          .attr("href");
+          //div.PromoSmall-titleContainer a
+          result.headline = $(element).find('div.PromoSmall-title a').text();
+          console.log(`CT: ${blogCount} TITLE: ${result.headline}`);
+          result.summary = $(element).find('div.PromoSmall-description').text();
+          console.log(`SUMMARY: ${result.summary}`);
+          result.link = $(element).find('div.PromoSmall-title a').attr('href');
+          console.log(`LINK: ${result.link}`);
+            // Add the text and href of every link, and save them as properties of the result object
+            // result.title = 
+              // .find("a")
+              // .text();
 
-        // Create a new BlogPost using the `result` object built from scraping
-        db.BlogPost.create(result)
-          .then(function(dbBlogPost) {
-            // View the added result in the console
-            console.log(dbBlogPost);
-          })
-          .catch(function(err) {
-            // If an error occurred, log it
-            console.log(err);
-          });
-      });
+            // result.link = $(this).attr("href");
+            //   // .children("a")
+            //   // .attr("href");
+
+            // console.log(result.title);
+            // console.log(result.link);
+
+            // Create a new BlogPost using the `result` object built from scraping
+            db.BlogPost.create(result)
+              .then(function(dbBlogPost) {
+                // View the added result in the console
+                console.log(dbBlogPost);
+              })
+              .catch(function(err) {
+                // If an error occurred, log it
+                console.log(err);
+              });
+
+        });
 
       // Send a message to the client
       res.send("Scrape Complete");
--- server.js
+++ server.js
@@ -2,11 +2,7 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
+
 
 // Require all models
 var db = require("./models");
@@ -33,6 +29,7 @@ app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/blogs", { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
 
 // Routes
 // =============================================================
